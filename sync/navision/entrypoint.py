fe916f863e22b245869526e255597b0c6fd363a7
Alisson Silveira <alissonsilveira@MacBook-Pro.local>
2025-10-07 14:42:24 -0300

    test

--- sync/navision/entrypoint.py
+++ sync/navision/entrypoint.py
@@ -2,7 +2,7 @@ from prefect import flow
 from claim import flow_sync_case_object, flow_sync_single_case_object
 
 
-@flow
+@flow(log_prints=True) # type: ignore
 def main_flow():
     """
     Main Prefect flow for syncing Salesforce cases to Navision.
@@ -10,6 +10,6 @@ def main_flow():
     Args:
         salesforce_case_number: Specific case number to sync from UI (optional)
     """
-    
+    print("flow ok")
     #flow_sync_single_case_object(salesforce_case_number)
-    flow_sync_case_object()
+    #flow_sync_case_object()
